096243b2986648d6bf2dc49616a959c1bdc9d238
Kahan <kdesai6826@conestogac.on.ca>
2022-11-22 16:06:25 -0500

    Made changes for possible cases in isosceles triangle, found through negative tests

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -12,7 +12,8 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 		result = "Equilateral triangle";
 	}
 	else if ((side1 == side2 && side1 != side3) || 
-		(side1 == side3 && side1 != side2))
+		(side1 == side3 && side1 != side2) || (side2 == side3 && side1 != side3))
+	// Change (Kahan) : Check point of side2 and side 3 being equal
 	{
 		result = "Isosceles triangle";
 	}
