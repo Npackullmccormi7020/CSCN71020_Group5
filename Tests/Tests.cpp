f6b9056f3bdb0f0b3e9067de92e2058b6ead6039
Kahan <kdesai6826@conestogac.on.ca>
2022-11-22 16:05:04 -0500

    Added and organized tests for triangleSolver, for all the cases

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -7,11 +7,65 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Tests
 {
-	TEST_CLASS(Tests)
+	TEST_CLASS(TriangleSolverTests)
 	{
 	public:
 		
-		TEST_METHOD(trianglesolver)
+		TEST_METHOD(TestA1)
+		{
+			char* result = "";
+			result = analyzeTriangle(-1, 1, 1);
+			Assert::AreEqual("Not a triangle", result);
+
+			// last test: Passed! \o/
+		}
+
+		TEST_METHOD(TestA2)
+		{
+			char* result = "";
+			result = analyzeTriangle(1, -1, 1);
+			Assert::AreEqual("Not a triangle", result);
+
+			// last test: Passed! \o/
+		}
+
+		TEST_METHOD(TestA3)
+		{
+			char* result = "";
+			result = analyzeTriangle(1, 1, -1);
+			Assert::AreEqual("Not a triangle", result);
+
+			// last test: Passed! \o/
+		}
+
+		TEST_METHOD(BVATestA1)
+		{
+			char* result = "";
+			result = analyzeTriangle(0, 1, 1);
+			Assert::AreEqual("Not a triangle", result);
+
+			// last test: Passed! \o/
+		}
+
+		TEST_METHOD(BVATestA2)
+		{
+			char* result = "";
+			result = analyzeTriangle(1, 0, 1);
+			Assert::AreEqual("Not a triangle", result);
+
+			// last test: Passed! \o/
+		}
+
+		TEST_METHOD(BVATestA3)
+		{
+			char* result = "";
+			result = analyzeTriangle(1, 1, 0);
+			Assert::AreEqual("Not a triangle", result);
+
+			// last test: Passed! \o/
+		}
+
+		TEST_METHOD(TestB1)
 		{
 			char* result = "";
 			result = analyzeTriangle(2, 2, 2);
@@ -19,6 +73,55 @@ namespace Tests
 
 			// last test: Passed! \o/
 		}
+
+		
+		TEST_METHOD(TestC1)
+		{
+			char* result = "";
+			result = analyzeTriangle(2, 3, 4);
+			Assert::AreEqual("Scalene triangle", result);
+
+			// last test: Passed! \o/
+		}
+
+		TEST_METHOD(TestD1)
+		{
+			char* result = "";
+			result = analyzeTriangle(2, 2, 3);
+			Assert::AreEqual("Isosceles triangle", result);
+
+			// last test: Passed! \o/
+
+		}
+
+		TEST_METHOD(TestD2)
+		{
+			char* result = "";
+			result = analyzeTriangle(2, 3, 2);
+			Assert::AreEqual("Isosceles triangle", result);
+
+			// last test: Passed! \o/
+
+		}
+
+		TEST_METHOD(TestD3)
+		{
+			char* result = "";
+			result = analyzeTriangle(2, 3, 3);
+			Assert::AreEqual("Isosceles triangle", result);
+
+			// Found that one condition was missing when side2 and side3 were equal, now solved (refer triangleSolver.c)
+			// last test: Passed! \o/
+
+		}
+
+
+	};
+
+	TEST_CLASS(OtherTests)
+	{
+	public:
+
 
 		TEST_METHOD(rectanglesolver)
 		{
