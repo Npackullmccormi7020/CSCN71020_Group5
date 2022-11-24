b12f1579239a98f326fceed0a4457abfed6f1823
Girish Bhuteja <Gbhuteja5688@conestogac.on.ca>
2022-11-24 17:08:57 -0500

    Changed the datatype to double and added function call statement of the function isTriangle().

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -1,4 +1,4 @@
 #pragma once
 void printWelcome();
 int printShapeMenu();
-int* getTriangleSides(int* triangleSides);
+double* getTriangleSides(double* triangleSides);         //changed(GIRISH) : changed the datatype from int to double
