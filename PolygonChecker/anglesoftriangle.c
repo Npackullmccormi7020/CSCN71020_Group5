f93f23371a1073929350fe9a4fb8d110c6208433
Kahan <kdesai6826@conestogac.on.ca>
2022-11-29 17:52:18 -0500

    Made the function anglesoftriangle work!

--- PolygonChecker/anglesoftriangle.c
+++ PolygonChecker/anglesoftriangle.c
@@ -2,16 +2,29 @@
 
 #include <stdio.h>
 #include <math.h>
+
+/*
+Revision history:
+2022-11-17: Created by Srujan Reddy Vadde
+2022-11-24: Updated the function (SrujanReddy)
+2022-11-29: Modified to eliminate the errors and make it functional (Kahan)
+*/
+
 void anglesoftriangle(double a, double b, double c)
 {
     double A, B, C;
-    double angleA, angleB, angleC;
+    
     A = ((a * a) + (b * b) - (c * c)) / 2*(a*b);
     B = ((b * b) + (c * c) - (a * a)) / 2*(b*c);
     C = ((c * c) + (a * a) - (b * b)) / 2*(a*c);
 
-    angleA = acos(A);
+    // Change (Kahan) : defined and initialized the doubles for angles.
+    double angleA = acos(A);
+    double angleB = acos(B);
+    double angleC = acos(C);
+
+    // Change (Kahan) : Just minor change in syntax adjustment to remove obvious error.
     printf("%lf", angleA);
-    printf("lf", angleB);
-    printf("lf", angleC);
+    printf("%lf", angleB);
+    printf("%lf", angleC);
 }
