68f05f1d9f6e933cdb14f7d4205a356054cff1ea
Girish Bhuteja <Gbhuteja5688@conestogac.on.ca>
2022-12-01 17:43:02 -0500

    Changed isTriangle() to make it function properly.

--- PolygonChecker/isTriangle.c
+++ PolygonChecker/isTriangle.c
@@ -13,7 +13,7 @@ Revision history:
 char* isTriangle(double side1, double side2, double side3)
 {
     char* statement = "";
-    if ((side1 + side2) >= side3 || (side2 + side3) >= side1 || (side3 + side1) >= side2)
+    if ((side1 + side2) >= side3 && (side2 + side3) >= side1 && (side3 + side1) >= side2)
     {
         statement = "Triangle is possible";
         printf("\n%s", statement);
